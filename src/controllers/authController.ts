723fb717b383b89369e495d132b076dc66703881
abbasibneakram <abbasibneakram@gmail.com>
2024-10-16 16:48:42 +0500

    added custom errors

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { signupService, loginService } from '../services/authService'
+import { NotFoundError, UnauthorizedError } from '../utils/errors'
 
 export const signup = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -15,14 +16,15 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     try {
         const { email, password } = req.body
         const token = await loginService(email, password)
-        res.status(200).json({ success: true, token })
+        res.status(200).json({ token })
     } catch (error: any) {
-        if (error.message === 'No user found!') {
-            res.status(404).json({ message: 'User not found' })
+        if (error instanceof NotFoundError) {
+            res.status(404).json({ message: error.message })
         }
-        if (error.message === 'Invalid credentials') {
-            res.status(401).json({ message: 'Invalid credentials' })
+        if (error instanceof UnauthorizedError) {
+            res.status(401).json({ message: error.message })
         }
+        console.error('Unhandled error:', error)
         res.status(500).json({ message: 'Internal server error' })
     }
 }
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import User from '../models/userModel'
 import { IUser } from '../interfaces/userInterface'
+import { NotFoundError, UnauthorizedError } from '../utils/errors'
 
 export const signupService = async (
     username: string,
@@ -27,9 +28,15 @@ export const loginService = async (
     password: string
 ): Promise<string> => {
     const user = await User.findOne({ where: { email } })
-    if (!user) throw new Error('No user found!')
-    if (!(await bcrypt.compare(password, user.password)))
-        throw new Error('Invalid credentials')
+    if (!user) {
+        throw new NotFoundError('User not found')
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+        console.log('Login Service: Invalid credentials')
+        throw new UnauthorizedError('Invalid credentials')
+    }
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, {
         expiresIn: '1h',
